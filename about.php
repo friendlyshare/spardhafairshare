c711e8845818f97dd457aacc6f81fcbe2f45f928
shrruthe <32519790+shrruthe@users.noreply.github.com>
2017-10-05 12:39:41 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Template 15</title>
+<meta name="description" content="A description of your website">
+<meta name="keywords" content="keyword1, keyword2, keyword3">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="left_section">
+    <div class="common_content">
+        <h2>About</h2>
+        <hr>
+        <p><ul>
+		<li>FairShare provides an online marketplace for shippers and Carriers to benefit from unutilised truck loading spaces</li>
+<li> Each day countries and companies around the world are realizing the importance and logic of cooperation </li>
+ <li>Online marketplace such as FAIRSHARE can be the first step</li>
+  <li>Our platform monitors available trucks with empty spaces,detects shippers needs and produces database with essential information to
+ instigate business transactions.</li>
+  <li>Yes,our FAIRSHARE  can reduce wastage of manpower and gas too.Every year some tons of carbondioxide emissions are released 
+ due to driving empty loaded vehicles.</li>
+  <li>Thus,our website aims to improve the efficiency and overall profit in the transport industry by allowing customers to book empty vehicle 
+ spaces in an affordable and convinient way.</li>
+  <li>The transport sector will benifit from this as FAIRSHARE continues to optimize the existing assets. </li>
+ <br>
+ </ul>
+      </div>
+	  </div>
+	  </body>
+	  </html>
