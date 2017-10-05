8492ce5f175ebc886509f380e934e29b7793aac9
DeepthiRudraraju <32528864+DeepthiRudraraju@users.noreply.github.com>
2017-10-05 12:32:52 +0530

    Add files via upload

---
+++ ClientReg.php
@@ -0,0 +1,140 @@
+
+<body>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>fairshare</title>
+<meta name="description" content="A description of your website">
+<meta name="keywords" content="keyword1, keyword2, keyword3">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+
+</head>
+<div id="wrapper"> 
+  
+  <div id="header">
+    <h1>FAIRSHARE logistics.</h1>
+    <p>An online marketplace.</p>
+  </div>
+
+  <div id="page_content">
+  
+  <div class="navigation">
+  <ul>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Contact</a></li>
+  <li><a href="#">New Page 1</a></li>
+  <li><a href="#">New Page 2</a></li>
+  <li><a href="#">New Page 3</a></li>
+  <li><a href="#">New Page 4</a></li>
+  </ul>
+  </div>
+
+    <div class="left_section">
+    <div class="common_content">
+       <form name="f3" action="">
+	 
+<style>
+input[type=text], select, textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+}
+input[type=submit] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+.container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+</style>
+
+
+
+<div class="container">
+  <form action="/action_page.php">
+    <label for="fname">Name:</label>
+    <input type="text" id="cname" name="cname" placeholder="Your name..">
+
+    <label for="fage">age:</label>
+    <input type="text" id="cage" name="cage" placeholder="yourr age..">
+
+	<label for="femail">Email:</label>
+    <input type="text" id="cemail" name="ceamil" placeholder="yourr mail id..">
+
+	<label for="femail">Adhar:</label>
+    <input type="text" id="cadhar" name="cadhar" placeholder="yourr adhar number plz..">
+
+  <label for="fmob">Mobile:</label>
+    <input type="text" id="cmob" name="cmob" placeholder="yourr mobile number.">
+
+  <label for="Password">Password:</label>
+    <input type="text" id="cpwd" name="cpwd" placeholder="set a password plz.">
+
+  
+  
+    
+    <input type="submit" value="Submit">
+  </form>
+</div>
+
+
+      </div>
+      
+      <div class="top_content border_none">
+        <div class="column_one">
+          
+        <div class="column_two border_left">
+        
+        </div>
+      </div>
+    </div>
+    
+    <div class="right_side_bar"> 
+      
+      <div class="col_1">
+      
+        <div class="box">
+         
+        </div>
+      </div>
+      
+      <div class="col_1">
+       
+        <div class="box">
+         
+        
+        </div>
+      </div>
+      
+    </div>
+    
+    <div class="clear"></div>
+
+  <!--start footer from here-->
+  <div id="footer">Copyright &copy; 2014. Design by <a href="http://www.htmltemplates.net" target="_blank">HTML Templates</a><br>
+  
+  <!--DO NOT remove footer link-->
+  <!--Template designed by--><a href="http://www.htmltemplates.net"><img src="images/footer.gif" class="copyright" alt="http://www.htmltemplates.net"></a></div>
+  
+  <!--/. end footer from here-->
+  </div>
+
+</div>
+
+</body>
+</html>
