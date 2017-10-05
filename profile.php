4155c6180c14a3d4fb6f6952f5c9c18ad2bec8cc
shrruthe <32519790+shrruthe@users.noreply.github.com>
2017-10-06 01:50:54 +0530

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<h2 style="text-align:center">User Profile Card</h2>
+<style="text-align:center">
+<div class="card">
+  <center><img src="C:\Users\srkrtech41\Desktop\empty profile.jpg" style="width:10%">
+  <div class="cointaner">
+  <h1>John Doe</h1>
+  <p class="title">owner, Example</p>
+  <p>rajahmundry</p>
+ <p><button>Contact</button></p></center>
+ </style>
+</div>
+</div>
+
+</body>
+</html>
